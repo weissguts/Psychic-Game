e91cf4abbe63e2177579356f70015673771ce089
Daniel Hernandez <weissguts@gmail.com>
2017-12-30 16:10:22 -0700

    Working text box and submit button. Updates properly. Need to write js functions to update guess wins/losses/remaining.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,39 @@
 $(document).ready(function() {
     //Variables
+    var guessLetterStart = "";
+    var possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     var wins = 0;
     var losses = 0;
-    var guessesLeft = 0;
-    var guessesSoFar = "";
+    var guessesLeft = 10;
+    var guessesSoFarCount = 0;
 
+
+
+
+
+    $('#guessButton').click(function (event) {
+        var guess = $('#guessBoxInput').val();
+        $('#guessSoFar').text("Guesses so far Test: " + guess);
+    });
+
+    //Main Functions
+    function randomLetter() {
+        for (var i = 0; i < 1; i++) {
+            guessLetterStart += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+        }
+    };
+
+
+    //JQ to replace HTML
+    randomLetter();
+
+
+
+    $('#guessLetterStart').text("Guess what letter I'm thinking of: " + guessLetterStart);
+    $('#wins').text("Wins: " + wins);
+    $('#losses').text("Losses: " + losses);
+    $('#guessesLeft').text("Guesses left: " + guessesLeft);
 
 
 });
+
